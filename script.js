35f598568b3c4a9a8c0894d1d5cb69e2fee247e1
ayushnemmaniwar <68007760+ayushnemmaniwar@users.noreply.github.com>
2025-03-01 08:58:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+document.getElementsByTagName("input")[0].addEventListener("click", function() {
+    const selectedItem = document.getElementById("colorSelect")
+    if (selectedItem.selectedIndex !== -1) {
+        selectedItem.remove(selectedItem.selectedIndex);
+    }
+}) 
